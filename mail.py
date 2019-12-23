1dd4f5ea3775b735a1efb54f1412a7c8635fbcae
ernavdeepkr <50796185+ernavdeepkr@users.noreply.github.com>
2019-12-23 15:53:37 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -1,15 +1,16 @@
-#usr/bin
+#!usr/bin/python
 #sending email using python3
 
 import smtplib 
-# list of email_id to send the mail 
+
+# list of email_idof users to send the mail 
 li = ["abc", "def", "xyz"] 
 
 for i in range(len(li)): 
 	s = smtplib.SMTP('smtp.gmail.com', 587) 
 	s.starttls() 
-	s.login("x", "y") 
+	s.login("sender_email_id", "password") 
 	message = "I m learning python so could u help me to continue further "
-	s.sendmail("x", li[i], message)
+	s.sendmail("sender_email_id", li[i], message)
     s.sendmail()
 	s.quit()
